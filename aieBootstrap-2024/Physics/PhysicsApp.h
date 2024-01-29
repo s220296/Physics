35b81ef8413acb0197687e6afc875035074c94c5
s220296 <s220296@students.aie.edu.au>
2024-01-29 11:21:34 +1100

    changed properties of physics project

---
+++ aieBootstrap-2024/Physics/PhysicsApp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Application.h"
+#include "Renderer2D.h"
+
+class PhysicsApp : public aie::Application {
+public:
+
+	PhysicsApp();
+	virtual ~PhysicsApp();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+protected:
+
+	aie::Renderer2D*	m_2dRenderer;
+	aie::Font*			m_font;
+};
