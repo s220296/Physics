c6f1ba541ed6dd2965fd977637703542ba85c990
s220296 <s220296@students.aie.edu.au>
2024-01-29 12:20:53 +1100

    added fps counter

--- aieBootstrap-2024/Physics/PhysicsApp.cpp
+++ aieBootstrap-2024/Physics/PhysicsApp.cpp
@@ -4,6 +4,8 @@
 #include "Input.h"
 #include "Gizmos.h"
 #include "glm/glm.hpp"
+#include "glm/ext.hpp"
+#include "string"
 
 PhysicsApp::PhysicsApp() {
 
@@ -15,6 +17,8 @@ PhysicsApp::~PhysicsApp() {
 
 bool PhysicsApp::startup() {
 	
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
+
 	m_2dRenderer = new aie::Renderer2D();
 
 	// TODO: remember to change this when redistributing a build!
@@ -36,6 +40,8 @@ void PhysicsApp::update(float deltaTime) {
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
+	aie::Gizmos::clear();
+
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
@@ -54,10 +60,22 @@ void PhysicsApp::draw() {
 	m_2dRenderer->setUVRect(0, 0, 1, 1);
 	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
 
+	aie::Gizmos::add2DCircle(glm::vec2(0), 3.f, 15, glm::vec4(1));
 
+	static float aspectRatio = 16.f / 9.f;
+	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100, 
+		-100 / aspectRatio, 100 / aspectRatio, -1.f, 1.f));
 
 	// output some text, uses the last used colour
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// my fps counter
+	//m_2dRenderer->drawText(m_font, std::to_string(getFPS()).c_str(), 640.f, 360.f);
+
+	// jesse's fps counter
+	char fps[32];
+	sprintf_s(fps, 32, "FPS: %i", getFPS());
+	m_2dRenderer->drawText(m_font, fps, 0.f, getWindowHeight() - 32);
 
 	// done drawing sprites
 	m_2dRenderer->end();
